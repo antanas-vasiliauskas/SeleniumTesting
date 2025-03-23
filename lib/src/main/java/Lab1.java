3443fbaed34ed5c7c65192f45c991fa90a63d576
Vasiliauskas Antanas <antanas.vasiliauskas@mif.stud.vu.lt>
2025-03-23 16:17:37 +0200

    first commit

---
+++ lib/src/main/java/Lab1.java
@@ -0,0 +1,108 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration; // Import Duration for Selenium 4.x
+import java.util.List;
+
+public class Lab1 {
+    public static void main(String[] args) {
+        // Set the path to your ChromeDriver executable
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Use Duration for timeout
+
+        try {
+            // 1. Open the website
+            driver.get("https://demowebshop.tricentis.com/");
+
+            // 2. Click on 'Gift Cards' in the left menu
+            driver.findElement(By.linkText("Gift Cards")).click();
+
+            // 3. Select a product with a price higher than 99
+            List<WebElement> giftcardProducts = driver.findElements(By.xpath("//div[contains(@class, 'product-item')]"));
+            for (WebElement product : giftcardProducts) {
+                String priceText = product.findElement(By.xpath(".//span[contains(@class, 'price')]")).getText();
+                double price = Double.parseDouble(priceText.replace("$", "").replace(",", ""));
+                if (price > 99) {
+                	product.findElement(By.xpath(".//h2[contains(@class, 'product-title')]//a")).click();
+                    break;
+                }
+            }
+
+            // 4. Fill in the fields 'Recipient's Name:' and 'Your Name:'
+            driver.findElement(By.xpath("//input[starts-with(@id, 'giftcard_') and contains(@id, '_RecipientName')]")).sendKeys("Recipient Name");
+            driver.findElement(By.xpath("//input[starts-with(@id, 'giftcard_') and contains(@id, '_SenderName')]")).sendKeys("Your Name");
+
+            // 5. Enter '5000' in the 'Qty' text field
+            WebElement qtyField = driver.findElement(By.xpath("//input[starts-with(@id, 'addtocart_') and contains(@id, '_EnteredQuantity')]"));
+            qtyField.clear();
+            qtyField.sendKeys("5000");
+
+            // 6. Click the 'Add to cart' button
+            driver.findElement(By.xpath("//input[starts-with(@id, 'add-to-cart-button-')]")).click();
+            wait.until(ExpectedConditions.textToBePresentInElementLocated(
+            	By.xpath("//div[contains(@class, 'bar-notification') and contains(@class, 'success')]"), "shopping cart")
+            );
+
+            // 7. Click the 'Add to wish list' button
+            driver.findElement(By.xpath("//input[starts-with(@id, 'add-to-wishlist-button-')]")).click();
+            wait.until(ExpectedConditions.textToBePresentInElementLocated(
+            	By.xpath("//div[contains(@class, 'bar-notification') and contains(@class, 'success')]"), "wishlist")
+            );
+
+            // 8. Click on 'Jewelry' in the left menu
+            driver.findElement(By.linkText("Jewelry")).click();
+
+            // 9. Click the 'Create Your Own Jewelry' link
+            driver.findElement(By.linkText("Create Your Own Jewelry")).click();
+
+            // 10. Select the following values:'Material' - 'Silver 1mm', 'Length in cm' - '80', 'Pendant' - 'Star'
+            driver.findElement(By.xpath("//select[contains(@id, 'product_attribute_71_9_15')]")).sendKeys("Silver (1 mm)");
+            WebElement lengthField = driver.findElement(By.xpath("//input[@id='product_attribute_71_10_16']"));
+            lengthField.clear();
+            lengthField.sendKeys("80");
+            driver.findElement(By.xpath("//input[@id='product_attribute_71_11_17_50']")).click();
+
+            // 11. Enter '26' in the 'Qty' text field
+            WebElement jewelryQtyField = driver.findElement(By.xpath("//input[starts-with(@id, 'addtocart_') and contains(@id, '_EnteredQuantity')]"));
+            jewelryQtyField.clear();
+            jewelryQtyField.sendKeys("26");
+
+            // 12. Click the 'Add to cart' button
+            driver.findElement(By.xpath("//input[starts-with(@id, 'add-to-cart-button-')]")).click();
+            wait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//div[contains(@class, 'bar-notification') and contains(@class, 'success')]"), "shopping cart"));
+
+            // 13. Click the 'Add to wish list' button
+            driver.findElement(By.xpath("//input[starts-with(@id, 'add-to-wishlist-button-')]")).click();
+            wait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//*[contains(@class, 'bar-notification') and contains(@class, 'success')]"), "wishlist"));
+            // 14. Click the 'Wishlist' link at the top of the page
+            driver.findElement(By.linkText("Wishlist")).click();
+
+            // 15. Check the 'Add to cart' checkboxes for both products
+            List<WebElement> addToCartCheckboxes = driver.findElements(By.xpath("//td[contains(@class, 'add-to-cart')]//input[@type='checkbox']"));
+            for (WebElement checkbox : addToCartCheckboxes) {
+                checkbox.click();
+            }
+
+            // 16. Click the 'Add to cart' button
+            driver.findElement(By.name("addtocartbutton")).click();
+            wait.until(ExpectedConditions.urlContains("/cart"));
+
+            // 17. Verify that the 'Sub-Total' value is '1002600.00'
+            String subTotal = driver.findElement(By.xpath("//*[contains(@class, 'cart-total-right')]")).getText();
+            System.out.println("subTotal: " + subTotal);
+            if (subTotal.equals("1002600.00")) {
+                System.out.println("Test passed: Sub-Total is correct");
+            } else {
+                System.out.println("Test failed: Sub-Total is incorrect");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+    }
+}
---
+++ lib/src/main/java/Lab2.java
@@ -0,0 +1,99 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class Lab2 {
+	public static void main(String[] args) {
+		Task1();
+		Task2();
+	}
+	public static void Task1() {
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        
+        try {
+        	//1. Open https://demoqa.com/.
+        	driver.get("https://demoqa.com/");
+        	//2. Close the cookie consent window.
+        	driver.findElement(By.xpath("//a[contains(@id,'close-fixedban')]")).click();
+        	//3. Select the "Widgets" tab.
+        	driver.findElement(By.xpath("//div[h5='Widgets']")).click();
+        	//4. Choose the "Progress Bar" menu item.
+        	driver.findElement(By.xpath("//span[text()='Progress Bar']")).click();
+        	//5. Click the "Start" button.
+        	driver.findElement(By.xpath("//button[text()='Start']")).click();
+        	//6. Wait until it reaches 100% and then click "Reset."
+        	wait.until(d -> 
+    			d.findElement(By.xpath("//div[@role='progressbar']")).getAttribute("aria-valuenow").equals("100")
+        	);
+        	driver.findElement(By.xpath("//button[text()='Reset']")).click();
+        	//7. Ensure that the progress bar is empty (0%).
+        	String resetValue = driver.findElement(By.xpath("//div[@role='progressbar']")).getAttribute("aria-valuenow");
+			System.out.println("Progress reset to: " + resetValue);
+        } finally {
+            driver.quit();
+        }
+    }
+
+    public static void Task2() {
+
+
+    	
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        
+        try {
+        	// 1. Open https://demoqa.com/.
+            driver.get("https://demoqa.com/");
+    		// 2. Close the cookie consent window.
+            driver.findElement(By.xpath("//a[contains(@id,'close-fixedban')]")).click();
+    		// 3. Select the "Elements" tab.
+            WebElement elementsButton = driver.findElement(By.xpath("//div[h5='Elements']"));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", elementsButton); // To avoid ElementClickInterceptedException
+            elementsButton.click();
+    		// 4. Choose the "Web Tables" menu item.
+            driver.findElement(By.xpath("//span[text()='Web Tables']")).click();
+    		// 5. Add enough elements to create a second page in the pagination.
+            for(int i = 0; i < 8; i++) {            	
+                driver.findElement(By.xpath("//button[@id='addNewRecordButton']")).click();
+                WebElement form = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                    By.xpath("//div[contains(@class,'modal-content')]")
+                ));
+                
+                form.findElement(By.xpath(".//input[@id='firstName']")).sendKeys("Test");
+                form.findElement(By.xpath(".//input[@id='lastName']")).sendKeys("User");
+                form.findElement(By.xpath(".//input[@id='userEmail']")).sendKeys("test@example.com");
+                form.findElement(By.xpath(".//input[@id='age']")).sendKeys("30");
+                form.findElement(By.xpath(".//input[@id='salary']")).sendKeys("50000");
+                form.findElement(By.xpath(".//input[@id='department']")).sendKeys("IT");
+                form.findElement(By.xpath(".//button[@id='submit']")).click();
+            }
+    		// 6. Navigate to the second page by clicking "Next."
+            WebElement nextButton = wait.until(ExpectedConditions.elementToBeClickable(
+            	By.xpath("//button[contains(@class, '-btn') and text()='Next']")
+            ));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", nextButton); // To avoid ElementClickInterceptedException
+            nextButton.click();
+    		// 7. Delete an element on the second page.
+            driver.findElement(By.xpath("(//span[@title='Delete'])[1]")).click();
+    		// 8. Ensure that pagination automatically returns to the first page and that the number of pages is reduced to one.
+            wait.until(d -> {
+                String currentPage = d.findElement(By.xpath("//span[@class='-pageInfo']//input[@type='number']")).getAttribute("value");
+                String totalPages = d.findElement(By.xpath("//span[@class='-pageInfo']//span[@class='-totalPages']")).getText();
+                return totalPages.equals("1"); // return currentPage.equals("1") && totalPages.equals("1");
+            });
+
+            // Print the pagination state
+            String currentPage = driver.findElement(By.xpath("//span[@class='-pageInfo']//input[@type='number']")).getAttribute("value");
+            String totalPages = driver.findElement(By.xpath("//span[@class='-pageInfo']//span[@class='-totalPages']")).getText();
+            System.out.println("Pagination state: Page " + currentPage + " of " + totalPages);
+        } finally {
+            driver.quit();
+        }
+    }
+}
